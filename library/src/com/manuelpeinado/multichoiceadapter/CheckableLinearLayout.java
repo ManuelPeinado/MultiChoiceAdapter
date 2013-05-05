6efa817c229d092be28040f7ddaf4d3108a10cb7
Manuel Peinado <manuel.peinado@gmail.com>
2013-05-05 16:17:10 +0200

    Remove state persistency, we let the adapter take care of this

--- library/src/com/manuelpeinado/multichoiceadapter/CheckableLinearLayout.java
+++ library/src/com/manuelpeinado/multichoiceadapter/CheckableLinearLayout.java
@@ -2,8 +2,6 @@ package com.manuelpeinado.multichoiceadapter;
 
 import android.content.Context;
 import android.graphics.drawable.Drawable;
-import android.os.Parcel;
-import android.os.Parcelable;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
 import android.view.View;
@@ -108,63 +106,5 @@ public class CheckableLinearLayout extends LinearLayout implements Checkable {
             drawable.setState(myDrawableState);
             invalidate();
         }
-    }
-
-    /**************************/
-    /** State persistency **/
-    /**************************/
-
-    static class SavedState extends BaseSavedState {
-        boolean checked;
-
-        SavedState(Parcelable superState) {
-            super(superState);
-        }
-
-        private SavedState(Parcel in) {
-            super(in);
-            checked = (Boolean) in.readValue(null);
-        }
-
-        @Override
-        public void writeToParcel(Parcel out, int flags) {
-            super.writeToParcel(out, flags);
-            out.writeValue(checked);
-        }
-
-        @Override
-        public String toString() {
-            return "CheckableLinearLayout.SavedState{" + Integer.toHexString(System.identityHashCode(this))
-                    + " checked=" + checked + "}";
-        }
-
-        public static final Parcelable.Creator<SavedState> CREATOR = new Parcelable.Creator<SavedState>() {
-            public SavedState createFromParcel(Parcel in) {
-                return new SavedState(in);
-            }
-
-            public SavedState[] newArray(int size) {
-                return new SavedState[size];
-            }
-        };
-    }
-
-    @Override
-    public Parcelable onSaveInstanceState() {
-        // Force our ancestor class to save its state
-        Parcelable superState = super.onSaveInstanceState();
-        SavedState ss = new SavedState(superState);
-
-        ss.checked = isChecked();
-        return ss;
-    }
-
-    @Override
-    public void onRestoreInstanceState(Parcelable state) {
-        SavedState ss = (SavedState) state;
-
-        super.onRestoreInstanceState(ss.getSuperState());
-        setChecked(ss.checked);
-        requestLayout();
     }
 }
